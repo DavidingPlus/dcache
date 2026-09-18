f65705c81f2d14cafbac250860d4975113b27381
DavidingPlus <davidingplus@qq.com>
2026-09-18 11:07:20 +0800

    link KCacheProtoTest with kcache instead of compiling protobuf itself

--- snippet/KCacheProtoTest/xmake.lua
+++ snippet/KCacheProtoTest/xmake.lua
@@ -1,11 +1,5 @@
-add_requires("protobuf-cpp")
-
 target("KCacheProtoTest")
     set_kind("binary")
-    set_languages("c++17")
-    add_packages("protobuf-cpp")
-
-    add_rules("protobuf.cpp")
-    add_files("../../src/proto/kcache.proto", {proto_public = true})
     add_files("main.cpp")
+    add_deps("kcache")
 target_end()
